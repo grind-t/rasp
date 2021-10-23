b5505bc86e36e52485c7a13d5894ed8e40fa952d
grind-t <grindt.gd@gmail.com>
2021-10-23 21:32:33 +0300

    add global swr config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { SWRConfig } from "swr";
+
+const api = "https://rasp.msfu.ru/api/";
+
+const swrOptions = {
+  fetcher: (resource, init) =>
+    fetch(`${api}${resource}`, init).then((res) => res.json()),
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig value={swrOptions}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
